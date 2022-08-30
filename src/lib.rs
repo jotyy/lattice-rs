f8061349b57fc4212bcf218a15bb79735e8bf2ad
Jotyy <jotyy318@gmail.com>
2022-08-30 11:03:06 +0800

    feat: filekey encrypt api

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,14 @@ pub mod signing;
 pub mod types;
 use api::file_key::FileKey;
 use napi::bindgen_prelude::*;
+use signing::Cryptography;
 
 #[macro_use]
 extern crate napi_derive;
 
 #[napi(object)]
 pub struct Decrypted {
-  pub sk: String,
+  pub private_key: String,
   pub is_gm: bool,
 }
 
@@ -21,10 +22,22 @@ pub fn decrypt_filekey(filekey: String, password: String) -> Result<Decrypted> {
 
   let res = match key_pair {
     Ok(value) => Ok(Decrypted {
-      sk: value.sk.to_str_radix(16),
+      private_key: value.sk.to_str_radix(16),
       is_gm: fk.is_gm,
     }),
     Err(_) => Err(Error::new(Status::InvalidArg, "wrong password".to_owned())),
   };
+  res
+}
+
+#[napi]
+pub fn encrypt_filekey(private_key: String, password: String, is_gm: bool) -> String {
+  let crypto = if is_gm {
+    Cryptography::GM
+  } else {
+    Cryptography::NIST
+  };
+  let file_key = FileKey::from_secret_key(private_key.as_bytes(), password.as_bytes(), crypto);
+  let res = serde_json::to_string(&file_key).unwrap();
   res
 }
