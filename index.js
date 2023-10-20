e479534b8754a9bab30635b31754bf2618333350
jotyy <jotyy318@gmail.com>
2023-10-20 15:39:11 +0800

    fix: revert version

--- index.js
+++ index.js
@@ -1,229 +1,243 @@
-const { existsSync, readFileSync } = require("fs");
-const { join } = require("path");
+const { existsSync, readFileSync } = require('fs')
+const { join } = require('path')
 
-const { platform, arch } = process;
+const { platform, arch } = process
 
-let nativeBinding = null;
-let localFileExisted = false;
-let loadError = null;
+let nativeBinding = null
+let localFileExisted = false
+let loadError = null
 
 function isMusl() {
   // For Node 10
-  if (!process.report || typeof process.report.getReport !== "function") {
+  if (!process.report || typeof process.report.getReport !== 'function') {
     try {
-      return readFileSync("/usr/bin/ldd", "utf8").includes("musl");
+      return readFileSync('/usr/bin/ldd', 'utf8').includes('musl')
     } catch (e) {
-      return true;
+      return true
     }
   } else {
-    const { glibcVersionRuntime } = process.report.getReport().header;
-    return !glibcVersionRuntime;
+    const { glibcVersionRuntime } = process.report.getReport().header
+    return !glibcVersionRuntime
   }
 }
 
 switch (platform) {
-  case "android":
+  case 'android':
     switch (arch) {
-      case "arm64":
-        localFileExisted = existsSync(join(__dirname, "rs.android-arm64.node"));
+      case 'arm64':
+        localFileExisted = existsSync(join(__dirname, 'rs.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require("./rs.android-arm64.node");
+            nativeBinding = require('./rs.android-arm64.node')
           } else {
-            nativeBinding = require("@latticelabs/rs-android-arm64");
+            nativeBinding = require('@latticelabs/rs-android-arm64')
           }
         } catch (e) {
-          loadError = e;
+          loadError = e
         }
-        break;
-      case "arm":
-        localFileExisted = existsSync(
-          join(__dirname, "rs.android-arm-eabi.node")
-        );
+        break
+      case 'arm':
+        localFileExisted = existsSync(join(__dirname, 'rs.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require("./rs.android-arm-eabi.node");
+            nativeBinding = require('./rs.android-arm-eabi.node')
           } else {
-            nativeBinding = require("@latticelabs/rs-android-arm-eabi");
+            nativeBinding = require('@latticelabs/rs-android-arm-eabi')
           }
         } catch (e) {
-          loadError = e;
+          loadError = e
         }
-        break;
+        break
       default:
-        throw new Error(`Unsupported architecture on Android ${arch}`);
+        throw new Error(`Unsupported architecture on Android ${arch}`)
     }
-    break;
-  case "win32":
+    break
+  case 'win32':
     switch (arch) {
-      case "x64":
+      case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, "rs.win32-x64-msvc.node")
-        );
+          join(__dirname, 'rs.win32-x64-msvc.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require("./rs.win32-x64-msvc.node");
+            nativeBinding = require('./rs.win32-x64-msvc.node')
           } else {
-            nativeBinding = require("@latticelabs/rs-win32-x64-msvc");
+            nativeBinding = require('@latticelabs/rs-win32-x64-msvc')
           }
         } catch (e) {
-          loadError = e;
+          loadError = e
         }
-        break;
-      case "ia32":
+        break
+      case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, "rs.win32-ia32-msvc.node")
-        );
+          join(__dirname, 'rs.win32-ia32-msvc.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require("./rs.win32-ia32-msvc.node");
+            nativeBinding = require('./rs.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require("@latticelabs/rs-win32-ia32-msvc");
+            nativeBinding = require('@latticelabs/rs-win32-ia32-msvc')
           }
         } catch (e) {
-          loadError = e;
+          loadError = e
         }
-        break;
-      case "arm64":
+        break
+      case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, "rs.win32-arm64-msvc.node")
-        );
+          join(__dirname, 'rs.win32-arm64-msvc.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require("./rs.win32-arm64-msvc.node");
+            nativeBinding = require('./rs.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require("@latticelabs/rs-win32-arm64-msvc");
+            nativeBinding = require('@latticelabs/rs-win32-arm64-msvc')
           }
         } catch (e) {
-          loadError = e;
+          loadError = e
         }
-        break;
+        break
       default:
-        throw new Error(`Unsupported architecture on Windows: ${arch}`);
+        throw new Error(`Unsupported architecture on Windows: ${arch}`)
     }
-    break;
-  case "darwin":
+    break
+  case 'darwin':
     switch (arch) {
-      case "x64":
-        localFileExisted = existsSync(join(__dirname, "rs.darwin-x64.node"));
+      case 'x64':
+        localFileExisted = existsSync(join(__dirname, 'rs.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require("./rs.darwin-x64.node");
+            nativeBinding = require('./rs.darwin-x64.node')
           } else {
-            nativeBinding = require("@latticelabs/rs-darwin-x64");
+            nativeBinding = require('@latticelabs/rs-darwin-x64')
           }
         } catch (e) {
-          loadError = e;
+          loadError = e
         }
-        break;
-      case "arm64":
-        localFileExisted = existsSync(join(__dirname, "rs.darwin-arm64.node"));
+        break
+      case 'arm64':
+        localFileExisted = existsSync(
+          join(__dirname, 'rs.darwin-arm64.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require("./rs.darwin-arm64.node");
+            nativeBinding = require('./rs.darwin-arm64.node')
           } else {
-            nativeBinding = require("@latticelabs/rs-darwin-arm64");
+            nativeBinding = require('@latticelabs/rs-darwin-arm64')
           }
         } catch (e) {
-          loadError = e;
+          loadError = e
         }
-        break;
+        break
       default:
-        throw new Error(`Unsupported architecture on macOS: ${arch}`);
+        throw new Error(`Unsupported architecture on macOS: ${arch}`)
     }
-    break;
-  case "freebsd":
-    if (arch !== "x64") {
-      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`);
+    break
+  case 'freebsd':
+    if (arch !== 'x64') {
+      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, "rs.freebsd-x64.node"));
+    localFileExisted = existsSync(join(__dirname, 'rs.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require("./rs.freebsd-x64.node");
+        nativeBinding = require('./rs.freebsd-x64.node')
       } else {
-        nativeBinding = require("@latticelabs/rs-freebsd-x64");
+        nativeBinding = require('@latticelabs/rs-freebsd-x64')
       }
     } catch (e) {
-      loadError = e;
+      loadError = e
     }
-    break;
-  case "linux":
+    break
+  case 'linux':
     switch (arch) {
-      case "x64":
+      case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, "rs.linux-x64-musl.node")
-          );
+            join(__dirname, 'rs.linux-x64-musl.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require("./rs.linux-x64-musl.node");
+              nativeBinding = require('./rs.linux-x64-musl.node')
             } else {
-              nativeBinding = require("@latticelabs/rs-linux-x64-musl");
+              nativeBinding = require('@latticelabs/rs-linux-x64-musl')
             }
           } catch (e) {
-            loadError = e;
+            loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, "rs.linux-x64-gnu.node")
-          );
+            join(__dirname, 'rs.linux-x64-gnu.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require("./rs.linux-x64-gnu.node");
+              nativeBinding = require('./rs.linux-x64-gnu.node')
             } else {
-              nativeBinding = require("@latticelabs/rs-linux-x64-gnu");
+              nativeBinding = require('@latticelabs/rs-linux-x64-gnu')
             }
           } catch (e) {
-            loadError = e;
+            loadError = e
           }
         }
-        break;
-      case "arm64":
+        break
+      case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, "rs.linux-arm64-musl.node")
-          );
+            join(__dirname, 'rs.linux-arm64-musl.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require("./rs.linux-arm64-musl.node");
+              nativeBinding = require('./rs.linux-arm64-musl.node')
             } else {
-              nativeBinding = require("@latticelabs/rs-linux-arm64-musl");
+              nativeBinding = require('@latticelabs/rs-linux-arm64-musl')
             }
           } catch (e) {
-            loadError = e;
+            loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, "rs.linux-arm64-gnu.node")
-          );
+            join(__dirname, 'rs.linux-arm64-gnu.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require("./rs.linux-arm64-gnu.node");
+              nativeBinding = require('./rs.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require("@latticelabs/rs-linux-arm64-gnu");
+              nativeBinding = require('@latticelabs/rs-linux-arm64-gnu')
             }
           } catch (e) {
-            loadError = e;
+            loadError = e
           }
         }
-        break;
+        break
+      case 'arm':
+        localFileExisted = existsSync(
+          join(__dirname, 'rs.linux-arm-gnueabihf.node')
+        )
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./rs.linux-arm-gnueabihf.node')
+          } else {
+            nativeBinding = require('@latticelabs/rs-linux-arm-gnueabihf')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
       default:
-        throw new Error(`Unsupported architecture on Linux: ${arch}`);
+        throw new Error(`Unsupported architecture on Linux: ${arch}`)
     }
-    break;
+    break
   default:
-    throw new Error(`Unsupported OS: ${platform}, architecture: ${arch}`);
+    throw new Error(`Unsupported OS: ${platform}, architecture: ${arch}`)
 }
 
 if (!nativeBinding) {
   if (loadError) {
-    throw loadError;
+    throw loadError
   }
-  throw new Error(`Failed to load native binding`);
+  throw new Error(`Failed to load native binding`)
 }
 
-const { decryptFilekey, encryptFilekey, signTransaction } = nativeBinding;
+const { decryptFilekey, encryptFilekey, signTransaction } = nativeBinding
 
-module.exports.decryptFilekey = decryptFilekey;
-module.exports.encryptFilekey = encryptFilekey;
-module.exports.signTransaction = signTransaction;
+module.exports.decryptFilekey = decryptFilekey
+module.exports.encryptFilekey = encryptFilekey
+module.exports.signTransaction = signTransaction
